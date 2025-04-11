5dd9af6e182a571e0611ead6b694c2b7df8d594b
Zeeshan-Islam1 <159948421+Zeeshan-Islam1@users.noreply.github.com>
2025-04-11 08:52:30 +0500

    update

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,8 @@
+function Footer () {
+    return (
+        <div className="Footer">
+            <p className="Footer">© 2025 Trendico, Inc. All rights reserved.</p>
+        </div>
+    );
+}
+export default Footer
